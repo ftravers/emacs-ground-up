175e550df0004238e1c8e2348d2c589a937df4ac
Fenton Travers <fenton.travers@gmail.com>
2019-07-26 07:21:01 -0700

    initial

---
+++ init.el
@@ -0,0 +1,17 @@
+;; interesting page:
+;; https://github.com/emacs-evil/evil-collection/issues/116
+;; ============= package archives ========
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+;; ============== use package =============
+(package-initialize)
+(when (not package-archive-contents) (package-refresh-contents))
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents) 
+      (package-install 'use-package)))
+(require 'use-package)
+(setq use-package-always-ensure t)    ;; download packages if not already downloaded
+
